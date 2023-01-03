32ee262d226b4eb0c9c2133c1f7d6c42e8e0f80b
Hillary Barth <hillarycbarth@gmail.com>
2023-01-02 19:56:28 -0800

    tetris stuff

--- Game/Game.cs
+++ Game/Game.cs
@@ -6,188 +6,29 @@ class Game
     public static readonly string Title = "Minimalist Game Framework";
     public static readonly Vector2 Resolution = new Vector2(512, 512);
 
-    // Define some constants controlling animation speed:
-    static readonly float Framerate = 10;
-    static readonly float PaddleSpeed = 350;
-    static readonly float BallSpeed = 120;
+    IGame currentGame = null;
 
-    // Load some textures when the game starts:
-    Texture texKnight = Engine.LoadTexture("knight.png");
-    Texture texBackground = Engine.LoadTexture("hillsbg.png");
-    Texture texBall = Engine.LoadTexture("ball.png");
-
-    // Load the font
     Font font = Engine.LoadFont("arcade.ttf", 24);
-
-    // #BA8475
-    Color colorPaddle = new Color(0xba, 0x84, 0x75);
-
-    // Keep track of the paddle and ball position
-    Vector2 paddlePosition = new Vector2(Resolution.X / 2, Resolution.Y - 60);
-    Vector2 ballPosition = Resolution / 2;
-    Vector2 direction = new Vector2(1, 1);
-
-    // Ball
-    bool ballLost = false;
-
-    // bricks
-    List<Brick> bricks;
-    float BrickWidth = 40;
-    float BrickHeight = 15;
-    int BrickSpacer = 10;
-    int hitCount = 0;
-    class Brick
-    {
-        //float Width = 25;
-        //float Height = 15;
-        public Vector2 position;
-        public bool hit;
-
-        public Brick(Vector2 position)
-        {
-            this.position = position;
-        }
-
-        public bool isColliding(Vector2 ballPosition, Texture texture)
-        {
-
-            if (position.X < ballPosition.X + texture.Width &&
-                position.X + 40 > ballPosition.X &&
-                position.Y < ballPosition.Y + texture.Height &&
-                position.Y + 15 > ballPosition.Y
-                )
-            //&& position.Y <= ballPosition.Y + texture.Height)
-            {
-                hit = true;
-                return true;
-            }
-            return false;
-        }
-    }
-
-    public Game()
-    {
-        generateBricks();
-    }
 
     public void Update()
     {
-        // Draw the background:
-        Engine.DrawRectSolid(new Bounds2(0, 0, Resolution.X, Resolution.Y), Color.Tan);
-        Engine.DrawTexture(texBackground, Vector2.Zero);
-
-        // Draw the title
-        Engine.DrawString("eclater", new Vector2(Resolution.X / 2, Resolution.Y / 4),
-            Color.DarkSalmon, font, TextAlignment.Center);
-
-        if (!ballLost)
+        if (currentGame == null)
         {
-            // draw the score
-            Engine.DrawString(hitCount.ToString(), new Vector2(25, Resolution.Y - 25),
-                Color.White, font, TextAlignment.Right);
-            // Draw the bricks
-            foreach (var brick in bricks)
-            {
-                if (!brick.hit)
-                {
-                    Bounds2 bounds = new Bounds2(brick.position, new Vector2(BrickWidth, BrickHeight));
-                    Engine.DrawRectSolid(bounds, Color.PaleVioletRed);
-                }
-            }
+            Engine.DrawString("1) Breakout", new Vector2(20, 20), Color.White, font);
+            Engine.DrawString("2) Tetris", new Vector2(20, 40), Color.White, font);
 
-            // Use the keyboard to control the paddle:
-            Vector2 moveOffset = Vector2.Zero;
-            if (Engine.GetKeyHeld(Key.Left))
+            if (Engine.GetKeyDown(Key.NumRow1))
             {
-                moveOffset.X -= 1;
+                currentGame = new BreakoutGame();
             }
-            if (Engine.GetKeyHeld(Key.Right))
+            else if (Engine.GetKeyDown(Key.NumRow2))
             {
-                moveOffset.X += 1;
+                currentGame = new TetrisGame();
             }
-            paddlePosition += moveOffset * PaddleSpeed * Engine.TimeDelta;
-
-            // Draw the paddle:
-            Engine.DrawRectSolid(new Bounds2(paddlePosition.X, paddlePosition.Y, 75, 15), colorPaddle);
-
-            // Handle ball direction
-            if (ballPosition.Y > Resolution.Y)
-            {
-                ballLost = true;
-            }
-            else if (ballPosition.Y + 1 + texBall.Height >= paddlePosition.Y
-                && ballPosition.Y + 1 <= paddlePosition.Y + 15
-                && ballPosition.X + texBall.Width >= paddlePosition.X
-                && ballPosition.X <= paddlePosition.X + 75)
-            {
-                direction.Y = -1;
-            }
-            else if (ballPosition.Y <= 0)
-            {
-                direction.Y = 1;
-            }
-
-            if (ballPosition.X + 1 + texBall.Width >= Resolution.X)
-            {
-                direction.X = -1;
-            }
-            else if (ballPosition.X + 1 <= 0)
-            {
-                direction.X = 1;
-            }
-
-            foreach (Brick brick in bricks)
-            {
-                if (!brick.hit && brick.isColliding(ballPosition, texBall))
-                {
-                    hitCount++;
-                    direction.Y *= -1;
-                };
-            }
-            ballPosition = updatePosition(direction, ballPosition, BallSpeed);
-
-            Engine.DrawTexture(texBall, ballPosition);
         }
         else
         {
-            Engine.DrawString("You lost the ball : (", Resolution / 2,
-                Color.DarkSalmon, font, TextAlignment.Center);
-            Engine.DrawString("Press enter to play again!",
-                new Vector2(Resolution.X / 2, Resolution.Y / 2 + 16),
-                Color.DarkSalmon, font, TextAlignment.Center);
-
-            if (Engine.GetKeyHeld(Key.Return))
-            {
-                ballLost = false;
-                ballPosition = Resolution / 2;
-            }
-
+            currentGame.Update();
         }
     }
-
-    public void generateBricks()
-    {
-        bricks = new List<Brick>();
-        // Let's do 5 rows of bricks, 10 each row
-        float startX = ((Resolution.X - (10 * BrickWidth + 9 * BrickSpacer)) / 2);
-        for (int i = 0; i < 5; i++)
-        {
-            float offsetY = i * BrickSpacer + startX;
-            for (int j = 0; j < 10; j++)
-            {
-                float offsetX = j * BrickSpacer + startX;
-                Vector2 pos = new Vector2(j * BrickWidth + offsetX, i * BrickHeight + offsetY);
-                Brick brick = new Brick(pos);
-                bricks.Add(brick);
-            }
-        }
-    }
-
-    public Vector2 updatePosition(Vector2 direction, Vector2 position, float speed)
-    {
-        position.X += direction.X * speed * Engine.TimeDelta;
-        position.Y += direction.Y * speed * Engine.TimeDelta;
-        return position;
-    }
-
 }
---
+++ Game/TetrisGame.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+
+class TetrisGame : IGame
+{
+
+    public static readonly float blockSize = 16;
+    public static readonly Vector2 Resolution = new Vector2(512, 512);
+
+    Vector2 bottomStart = new Vector2(0, 512 - 32);
+    Vector2 bottomEnd = new Vector2(512, 512 - 32);
+    Bounds2 bottomBounds;
+
+    Vector2 leftStart = new Vector2(32, 0);
+    Vector2 leftEnd = new Vector2(32, 512 - 32);
+
+    Vector2 rightStart = new Vector2(512 - 32, 0);
+    Vector2 rightEnd = new Vector2(512 - 32, 512 - 32);
+
+    // Tetronimo
+    Vector2 initialPosition = new Vector2((512) / 2, 16);
+    Tetronimo t;
+    List<Tetronimo> tetronimos;
+
+    float velocity = 1;
+
+    // Playfield size is 10 x 40, with 20 rows hidden from the player
+    // https://tetris.fandom.com/wiki/Playfield
+    // https://hackaday.com/2014/01/28/a-deep-dive-into-nes-tetris/
+    // need to create a grid
+
+    int[,] grid = new int[16, 10];
+
+    float count = 0;
+
+    // Load the font
+    Font font = Engine.LoadFont("arcade.ttf", 24);
+    public TetrisGame()
+    {
+        bottomBounds = new Bounds2(bottomStart, bottomEnd);
+        t = new Tetronimo(initialPosition);
+        tetronimos = new List<Tetronimo>();
+
+    }
+
+
+    public void Update()
+    {
+        count += 1;
+
+        if (count % 100 != 0)
+        {
+            return;
+        }
+        Engine.DrawString(count.ToString(), new Vector2(20, 20), Color.White, font);
+        Engine.DrawLine(bottomStart, bottomEnd, Color.BlueViolet);
+        Engine.DrawLine(leftStart, leftEnd, Color.BlueViolet);
+        Engine.DrawLine(rightStart, rightEnd, Color.BlueViolet);
+
+        foreach (var b in tetronimos)
+        {
+            Bounds2 bounds = new Bounds2(b.position * 16, b.size);
+            Engine.DrawRectSolid(bounds, Color.OldLace);
+        }
+
+        if (!t.stopped)
+        {
+            // Draw the Tetronimo
+            Engine.DrawRectSolid(t.bounds, t.color);
+            Console.WriteLine(t.bounds.ToString());
+
+            // Use the keyboard to control the block:
+            Vector2 moveOffset = Vector2.Zero;
+            if (Engine.GetKeyHeld(Key.Left))
+            {
+                moveOffset.X -= 1;
+            }
+            if (Engine.GetKeyHeld(Key.Right))
+            {
+                moveOffset.X += 1;
+            }
+            if (Engine.GetKeyHeld(Key.Down))
+            {
+                moveOffset.Y += 1;
+            }
+            if (Engine.GetKeyHeld(Key.Up))
+            {
+                moveOffset.Y -= 1;
+            }
+            //moveOffset.Y += 16 * velocity;
+
+            //if (Engine.GetKeyHeld(Key.Down))
+            //{
+            //    velocity = 5;
+            //}
+            //else
+            //{
+            //    velocity = 1;
+            //}
+
+
+            t.position += moveOffset;
+            t.bounds = new Bounds2(t.position, t.size);
+
+            bool colliding = false;
+            foreach (var b in tetronimos)
+            {
+                if (t.isColliding(b.bounds))
+                {
+                    colliding = true;
+                    break;
+                }
+            }
+
+            //if (t.position.Y >= bottomBounds.Position.Y - t.size.Y || colliding)
+            //{
+            //    // stop the current tetronimo
+            //    t.stopped = true;
+            //    tetronimos.Add(t);
+
+            //    // start again with a new block
+            //    t = new Tetronimo(initialPosition);
+            //    // reset velocity
+            //    velocity = 1;
+            //}
+        }
+
+    }
+}
+
